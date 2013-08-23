757fad4e3531c800ed10baa2562ae65f56831d92
Martin Daniel <martindaniel4@gmail.com>
2013-08-23 17:31:20 +0200

    Edit View

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+   consumer_key    : '729246522004.apps.googleusercontent.com',
+   consumer_secret : 'HA9F0yoBK1j_VDqHSj3zBeFT'
+}
--- serveur.js
+++ serveur.js
@@ -44,7 +44,7 @@ app.get('/auth/callback',
   passport.authenticate('google', { session: false, failureRedirect: '/login' }),
   function(req, res) { 
     req.session.access_token = req.user.accessToken;
-    res.redirect('/');
+    res.redirect('/list');
   });
 
 /*
@@ -61,7 +61,7 @@ res.render('welcome.ejs');
 });
 
 
-app.get('/login',function(req, res){
+app.get('/list',function(req, res){
 
 if(!req.session.access_token) return res.redirect('/auth');
   
@@ -82,17 +82,53 @@ app.get('/details/:id', function(req, res) {
   var accessToken = req.session.access_token;
   var google_calendar = new gcal.GoogleCalendar(accessToken);
   
-  google_calendar.events.list(req.params.id,{maxResults:5000},function(err, events) {
+  google_calendar.events.list(req.params.id,{maxResults:5000,sanitizeHtml: true, singleEvents: true, timeMax: moment().subtract('days',2).format()},function(err, events) {
     if(err) return res.send(500,err);
 
-    var count = _.countBy(events.items, function(d) 
+   events.items = events.items.filter(function(d) {return d.status != "cancelled";})
+
+
+var tab = [],
+    bad = [];
+
+    events.items.forEach(function(d, i) {
+
+      if (d.start.dateTime != undefined) {
+
+      tab.push({
+
+          date : moment(d.start.dateTime).format("YYYY-MM-DD"),
+          id: d.id
+
+              })
+
+      } else {
+
+        tab.push({
+          date: d.start.date,
+          id: d.id
+
+        })
+
+      }
+
+              });
+
+
+
+    var count = _.countBy(tab, function(d) 
           {
-            return moment(d.created, "YYYY-MM");
-
-
+            return moment(d.date).format("YYYY-MM-DD");
           });
 
-  res.render('details.ejs', {events: events, id: req.params.id, count: JSON.stringify(count)});
+    var minYear = moment(_.min(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
+        maxYear = moment(_.max(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
+        minVolume = _.min(count, function(d) {return d;});
+        maxVolume = _.max(count, function(d) {return d;});
+
+    //var aout = events.items.filter(function(d) {return moment(d.start.dateTime).format("YYYY-MM-DD") !=   "2013-08-23";});
+
+  res.render('details.ejs', {events: events, length: tab.length, id: req.params.id, count: JSON.stringify(count), minYear: JSON.stringify(minYear), maxYear: JSON.stringify(maxYear), maxVolume: JSON.stringify(maxVolume), minVolume: JSON.stringify(minVolume), tab: JSON.stringify(tab), bad: JSON.stringify(bad)});
 
   });
 

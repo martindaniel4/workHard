0af7150e68f2a315a5c2c467cc990c347fd15cbb
Martin Daniel <martindaniel4@gmail.com>
2013-08-26 19:53:00 +0200

    edit serveur

--- serveur.js
+++ serveur.js
@@ -81,15 +81,112 @@ app.get('/details/:id', function(req, res) {
 
   var accessToken = req.session.access_token;
   var google_calendar = new gcal.GoogleCalendar(accessToken);
-  
-  google_calendar.events.list(req.params.id,{maxResults:5000,sanitizeHtml: true, singleEvents: true, timeMax: moment().subtract('days',2).format()},function(err, events) {
-    if(err) return res.send(500,err);
+
+    google_calendar.events.list(req.params.id,{sanitizeHtml: true, maxResults: 2500, singleEvents: true, timeMax: moment().subtract('years',1).format()},function(err, events) {
+    
+var tab = [],
+    bad = [];
 
    events.items = events.items.filter(function(d) {return d.status != "cancelled";})
 
 
-var tab = [],
-    bad = [];
+    events.items.forEach(function(d, i) {
+
+      if (d.start.dateTime != undefined) {
+
+      tab.push({
+
+          date : moment(d.start.dateTime).format("YYYY-MM-DD"),
+          id: d.id
+
+              })
+
+      } else {
+
+        tab.push({
+          date: d.start.date,
+          id: d.id
+
+        })
+
+      }
+
+              });
+
+    var count = _.countBy(tab, function(d) 
+          {
+            return moment(d.date).format("YYYY-MM-DD");
+          });
+
+    var minYear = moment(_.min(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
+        maxYear = moment(_.max(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
+        minVolume = _.min(count, function(d) {return d;});
+        maxVolume = _.max(count, function(d) {return d;});
+
+    //var aout = events.items.filter(function(d) {return moment(d.start.dateTime).format("YYYY-MM-DD") !=   "2013-08-23";});
+
+  res.render('details.ejs', {events: events, length: tab.length, id: req.params.id, count: JSON.stringify(count), minYear: JSON.stringify(minYear), maxYear: JSON.stringify(maxYear), maxVolume: JSON.stringify(maxVolume), minVolume: JSON.stringify(minVolume), tab: JSON.stringify(tab), bad: JSON.stringify(bad)});
+
+
+
+  });
+
+});
+
+
+
+app.get('/details2/:id', function(req, res) {
+
+  function fetchEventWithToken(nextPageToken){
+
+  var accessToken = req.session.access_token;
+  var google_calendar = new gcal.GoogleCalendar(accessToken);
+
+  var options = {
+
+    maxResults:2000,
+    pageToken:nextPageToken
+  }
+
+  //Fetch events
+  google_calendar.events.list(req.params.id, {maxResults: options.maxResults}, function(err, events) {
+
+    console.log(events.nextPageToken);
+
+    //If there is more result, call the function again...
+    if(events.nextPageToken){
+
+      console.log("----------TROP d'EVENTS--------");
+
+      fetchEventWithToken(events.nextPageToken);
+    }
+
+    //Do something with events
+    console.log("---DO SOMETHING-");
+
+    // render view
+
+    renderView();
+
+  });
+
+
+  function renderView() {
+
+  res.render('welcome.ejs');
+
+  }
+
+}
+
+//Start the function
+fetchEventWithToken(null)
+
+  });
+
+
+
+function getEventsData() {
 
     events.items.forEach(function(d, i) {
 
@@ -115,21 +212,4 @@ var tab = [],
               });
 
 
-
-    var count = _.countBy(tab, function(d) 
-          {
-            return moment(d.date).format("YYYY-MM-DD");
-          });
-
-    var minYear = moment(_.min(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
-        maxYear = moment(_.max(tab, function(d) {return moment(d.date)}).date).format("YYYY"),
-        minVolume = _.min(count, function(d) {return d;});
-        maxVolume = _.max(count, function(d) {return d;});
-
-    //var aout = events.items.filter(function(d) {return moment(d.start.dateTime).format("YYYY-MM-DD") !=   "2013-08-23";});
-
-  res.render('details.ejs', {events: events, length: tab.length, id: req.params.id, count: JSON.stringify(count), minYear: JSON.stringify(minYear), maxYear: JSON.stringify(maxYear), maxVolume: JSON.stringify(maxVolume), minVolume: JSON.stringify(minVolume), tab: JSON.stringify(tab), bad: JSON.stringify(bad)});
-
-  });
-
-});
+}
